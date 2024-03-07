a69713e4447a5d2ffcec67ef14039af4bc8358fb
Adib Faramarzi <adibfara@gmail.com>
2024-03-07 21:32:50 +0330

    Create instagram animation

--- app/src/main/java/io/github/adibfara/instagram/animation/model/AnimationParams.kt
+++ app/src/main/java/io/github/adibfara/instagram/animation/model/AnimationParams.kt
@@ -6,9 +6,9 @@ class AnimationParams {
      val totalArcs = 30
      val eachAngle = 360f / totalArcs.toFloat()
      val startDelayAmount = 13L
-     val startDuration = 700
+     val startDuration = 800
      val endDuration = (startDuration * 0.6f).toInt()
-     val waitDelay: Long = 300
+     val waitDelay: Long = 200
      val startColor = Color(0xffa52c9d)
      val endColor = Color(0xfff7c269)
  }
